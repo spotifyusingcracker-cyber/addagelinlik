23b321db18c5ad222ff646cff0f4bb92c995a051
spotifyusingcracker-cyber <spotifyusingcracker@gmail.com>
2026-09-01 22:06:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,5 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-
-    prerender: {
-      enabled: true,
-      crawlLinks: true,
-    },
   },
 });
